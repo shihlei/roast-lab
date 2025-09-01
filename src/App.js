63668d9d9c489ff925abde6fd37e36d11d9ecbce
shihlei <shihlei121786@gmail.com>
2025-09-01 15:52:03 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+
+import beans1 from "./assets/beans1.jpg";
+import beans2 from "./assets/beans2.jpg";
+import beans3 from "./assets/beans3.jpg";
+import beans4 from "./assets/beans4.jpg";
+import beans5 from "./assets/beans5.jpg";
+
+import Directory from "./components/directory/directory.component";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  
+  // We know this is something will show up several times with a pattern
+  const categories = [
+    {
+      id: 1,
+      title: 'Africa',
+      imageUrl: beans1,
+    },
+    {
+      id: 2,
+      title: 'Asia Pacific',
+      imageUrl: beans2,
+    },
+    {
+      id: 3,
+      title: 'Latin America',
+      imageUrl: beans3,
+    },
+    {
+      id: 4,
+      title: 'Decaf',
+      imageUrl: beans4,
+    },
+    {
+      id: 5,
+      title: 'Samplers',
+      imageUrl: beans5,
+    },
+  ]
+  
+  return <Directory categories={categories} />;
 }
 
 export default App;
